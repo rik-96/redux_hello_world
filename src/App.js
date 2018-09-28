4fd71773d2f081f2f30a7039bf3794f6317d8615
rik <dk84864@gmail.com>
2018-09-28 23:10:05 +0530

    Updating source

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import ButtonGroup from "./ButtonGroup";
+import { store } from "./store";
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
   render() {
-    return <HelloWorld tech={this.state.tech} />;
+    console.log(store.getState());
+    return [
+      <HelloWorld key={1} tech={store.getState().tech} />,
+      <ButtonGroup key={2} technologies={["React", "Elm", "React-redux"]} />
+    ];
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { store } from './store';
 import registerServiceWorker from './registerServiceWorker';
+const render = () => ReactDOM.render(<App />,
+  document.getElementById('root'));
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.subscribe(render);
+render();
+
 registerServiceWorker();
